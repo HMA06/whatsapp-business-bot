35fbf96750d10cb615853230131d797911da5d2f
Hamza Aljbawi <aljbawihamza@outlook.com>
2025-12-25 13:44:53 +0300

    Refine CORS comment in main.ts
    Updated CORS comment for clarity.

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // تفعيل CORS ضروري جداً للسماح للفرونت إند باستلام الـ QR من نفق ngrok
+  // تفعيل CORS للسماح للفرونت إند بالتحدث مع السيرفر
   app.enableCors({
-    origin: '*', 
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-
-  // الباك إند يعمل على بورت 3001
   await app.listen(3001);
-  console.log(`Server started on http://localhost:3001`);
 }
 bootstrap();
